a4de941a702b25d966da9b26f3af2a1cdd215ac2
nicketm <nicketmauskar@gmail.com>
2023-01-09 19:24:49 -0600

    ManyLinks COMPLETE
    5x5x5 tower created

--- generate.py
+++ generate.py
@@ -1,6 +1,31 @@
 import pyrosim.pyrosim as pyrosim
 
+pyrosim.Start_SDF("boxes.sdf")
+length = 1
+width = 1
+height = 1
+x = 0 
+y = 0
+z = .5
+x1 = 0
+y1 = 1
+z1 = 1
+for k in range(0, 4): 
+    x = k
+    y = 0
+    z = .5
+    for j in range(1, 5): 
+        z = .5
+        y += 1
+        length = 1
+        width = 1
+        height = 1
+        for i in range(1, 10): 
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+            z+= 1
+            length = .9*length
+            width = .9*width
+            height = .9*height
+#pyrosim.Send_Cube(name="Box2", pos=[x1,y1,z1] , size=[length,width,height])
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
 pyrosim.End()
